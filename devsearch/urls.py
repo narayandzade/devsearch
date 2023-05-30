1a583bac29a8fc4263b880c08d3a6811516ed884
Narayan Zade <narayan.d.zade@gmail.com>
2023-05-30 22:27:02 +0530

    index route created in urls.py by Narayan

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Home Page")
+
 def projects(request):
     return HttpResponse("Here are our projects")
 
@@ -9,6 +12,7 @@ def project(request):
     return HttpResponse("Single Project")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
     path('projects/', projects, name="projects"),
     path('project/', project, name="project"),
 ]
