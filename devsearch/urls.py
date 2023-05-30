20dfb809ed858dbc7106f7d2a568c12dbd403e6f
Rishav Mandal <coder.rishav@gmail.com>
2023-05-30 22:42:52 +0530

    Project route urls separation in project app folder from root folder by Rishav & project dynamic route created by Rishav

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path,include
 
-def index(request):
-    return HttpResponse("Home Page")
-
-def projects(request):
-    return HttpResponse("Here are our projects")
-
-def project(request):
-    return HttpResponse("Single Project")
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
-    path('projects/', projects, name="projects"),
-    path('project/', project, name="project"),
+    path('', include('projects.urls')),
 ]
