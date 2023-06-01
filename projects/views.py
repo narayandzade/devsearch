90c94eec3c2d584c34433dd585c7adc3f4496146
Narayan Zade <narayan.d.zade@gmail.com>
2023-06-02 01:05:35 +0530

    single project dynamic pk issue solved by Narayan

--- projects/views.py
+++ projects/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 
 def projects(request):
     page = "projects"
-    page_id = 1
+    page_id = 3444
     context = { "page":page, "page_id":page_id }
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/project.html', str(pk))
+
+    context= {"pk": pk}
+    return render(request, 'projects/project.html', context)
