d07567132f47ee5a9ce86f7cef7f4b9bacd9c485
Narayan Zade <narayan.d.zade@gmail.com>
2023-06-02 00:06:34 +0530

    basic tempates added by Narayan

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our projects")
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse("Single Project" +' '+str(pk))
+    return render(request, 'project.html')
