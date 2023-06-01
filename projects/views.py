19cd99568f7e2d3d29f0b415c0f203aa5ddf28f5
Rishav Mandal <coder.rishav@gmail.com>
2023-06-02 01:01:43 +0530

    Templates Implemented by Rishav

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    page = "projects"
+    page_id = 1
+    context = { "page":page, "page_id":page_id }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'project.html')
+    return render(request, 'projects/project.html', str(pk))
