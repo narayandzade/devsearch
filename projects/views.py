1a09df402e65d360f7190cc72a07d9d94afc39c8
Narayan Zade <narayan.d.zade@gmail.com>
2023-06-05 00:12:20 +0530

    model query implemented by Narayan

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Project
+
+from .forms import ProjectForm
+
+
 
 def projects(request):
     page = "projects"
     page_id = 3444
-    context = { "page":page, "page_id":page_id }
+    projects= Project.objects.all()
+
+    context = { "page":page, "page_id":page_id ,"projects": projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
 
-    context= {"pk": pk}
-    return render(request, 'projects/project.html', context)
+    projectObj= Project.objects.get(id=pk)
+   
+    return render(request, 'projects/project.html', {"project": projectObj})
+
+def createProject(request):
+     
+     form = ProjectForm()
+     context={'form': form}
+     return render(request,'projects/add_project.html',context ) 
+
