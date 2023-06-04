72c02efe093ec52e84c10cdf8e2b1d41ae2f2ebb
Narayan Zade <narayan.d.zade@gmail.com>
2023-06-05 00:27:50 +0530

    new db query done

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project, Review, Tag
+from .models import Project, Review, Tag, PersonalDetails
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Review)
 admin.site.register(Tag)
+admin.site.register(PersonalDetails)
